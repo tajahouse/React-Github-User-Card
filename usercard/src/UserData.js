784b468ef76185ab4595e7d89068f32e1ea741d9
Taja House <ahlliahtaja@yahoo.com>
2020-05-07 16:56:23 -0700

    Added box to user card

--- usercard/src/UserData.js
+++ usercard/src/UserData.js
@@ -4,12 +4,12 @@ const UserData = ({users}) =>{
     return (
         <div className="user-wrapper">
             <h1>Name: {users.name}</h1>
-            <img src={users.avatar_url} alt="Profile Image"/>
+            <img src={users.avatar_url} alt="Profile Image" className="user-image"/>
+    <p>Location: {users.location}</p>
     <p>Followers: {users.followers}</p>
     <p>Following: {users.following}</p>
     <a href={users.html_url} alt="Profile url">Github Profile</a>
-
-
+    <p>Bio: {users.bio}</p>
         </div>
     )
 }
