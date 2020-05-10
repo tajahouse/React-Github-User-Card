00e60107e5d63d8bd1df9c4fd1b89e3937ae763e
Taja House <ahlliahtaja@yahoo.com>
2020-05-10 08:18:01 -0700

    Chagned h1 in friends and added background to css

--- usercard/src/components/Friends.js
+++ usercard/src/components/Friends.js
@@ -5,9 +5,9 @@ const Friends = ({friends}) =>{
     return (
         <div className="friends-wrapper">
             
-            <h1>Name: {friends.login}</h1>
+            <h1> {friends.login}</h1>
     <img src={friends.avatar_url} alt="Profile Image" className="friends-image"/>
-  <button><a href={friends.html_url} alt="Profile Repos">Repos</a></button>
+  <button className="repo-button"><a href={friends.html_url} alt="Profile Repos">Repos</a></button>
         </div>
     )
 }
