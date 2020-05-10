f292d2e29ad010273ec481214e113f1201f03b01
Taja House <ahlliahtaja@yahoo.com>
2020-05-10 09:01:06 -0700

    Changed friends button name to profile

--- usercard/src/components/Friends.js
+++ usercard/src/components/Friends.js
@@ -7,7 +7,7 @@ const Friends = ({friends}) =>{
             
             <h1> {friends.login}</h1>
     <img src={friends.avatar_url} alt="Profile Image" className="friends-image"/>
-  <button className="repo-button"><a href={friends.html_url} alt="Profile Repos">Repos</a></button>
+  <button className="repo-button"><a href={friends.html_url} alt="Profile Repos">Profile</a></button>
         </div>
     )
 }
